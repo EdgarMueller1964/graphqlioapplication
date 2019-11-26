c800bd8f430913f2bcc8921e5ee96d27b42316dd
EdgarMueller1964 <50403858+EdgarMueller1964@users.noreply.github.com>
2019-11-26 11:55:02 +0100

    application using separate graphlio libraries

--- src/main/java/com/thinkenterprise/graphqlioapplication/GraphqlioapplicationApplication.java
+++ src/main/java/com/thinkenterprise/graphqlioapplication/GraphqlioapplicationApplication.java
@@ -3,10 +3,18 @@ package com.thinkenterprise.graphqlioapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.thinkenterprise.graphqlio.EnableGraphQLIOLibraryModule;
+import com.thinkenterprise.graphqlio.server.gs.EnableGraphQLIOGsLibraryModule;
+import com.thinkenterprise.graphqlio.server.gts.EnableGraphQLIOGtsLibraryModule;
+import com.thinkenterprise.graphqlio.server.gtt.EnableGraphQLIOGttLibraryModule;
+import com.thinkenterprise.graphqlio.server.wsf.EnableGraphQLIOWsfLibraryModule;
+
+//import com.thinkenterprise.graphqlio.EnableGraphQLIOLibraryModule;
 
 @SpringBootApplication
-@EnableGraphQLIOLibraryModule
+@EnableGraphQLIOWsfLibraryModule
+@EnableGraphQLIOGttLibraryModule
+@EnableGraphQLIOGtsLibraryModule
+@EnableGraphQLIOGsLibraryModule
 public class GraphqlioapplicationApplication {
 	
 	public static void main(String[] args) {
