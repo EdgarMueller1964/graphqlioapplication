40f81ec898a5b4d86d0c4878b5cfd2816852af1e
kuehnert <kuehnert@MSGN04573.int.root.msg.ag>
2019-11-26 13:33:11 +0100

    redis embedded added

---
+++ src/main/java/com/thinkenterprise/graphqlioapplication/service/GraphQLIORedisService.java
@@ -0,0 +1,39 @@
+package com.thinkenterprise.graphqlioapplication.service;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.thinkenterprise.graphqlio.server.gs.keyvaluestore.GsGraphQLRedisService;
+
+@Profile("test")
+@Service
+public class GraphQLIORedisService implements ApplicationRunner, DisposableBean {
+
+	@Autowired
+	private GsGraphQLRedisService graphQLRedisService;
+
+	public GraphQLIORedisService() {
+	}
+
+	@Override
+	public void run(ApplicationArguments args) {
+		try {
+			this.graphQLRedisService.start();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		this.graphQLRedisService.stop();
+	}
+
+}
