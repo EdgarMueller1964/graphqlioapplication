11ed8f67f871e2856160fb91b42ba8591933e8e2
schengw <57491891+schengw@users.noreply.github.com>
2019-11-28 16:22:08 +0100

    unused imports deleted

--- src/main/java/com/thinkenterprise/graphqlioapplication/mutationresolver/MutationResolver.java
+++ src/main/java/com/thinkenterprise/graphqlioapplication/mutationresolver/MutationResolver.java
@@ -1,35 +1,37 @@
 package com.thinkenterprise.graphqlioapplication.mutationresolver;
 
-import java.util.List;
-
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.thinkenterprise.graphqlioapplication.domain.Route;
-import com.thinkenterprise.graphqlioapplication.domain.mutationinput.CreateRouteInput;
 import com.thinkenterprise.graphqlioapplication.domain.mutationinput.UpdateRouteInput;
 import com.thinkenterprise.graphqlioapplication.repository.RouteRepository;
- 
+
 @Component
-public class MutationResolver implements GraphQLMutationResolver{
-	
-private RouteRepository routeRepository;
+public class MutationResolver implements GraphQLMutationResolver {
+
+	private RouteRepository routeRepository;
 
 	public MutationResolver(RouteRepository routeRepository) {
-		this.routeRepository=routeRepository;
+		this.routeRepository = routeRepository;
 	}
+
 	@Transactional
-	public Route updateRoute(String flightNumber,UpdateRouteInput input) {
-		Route route =routeRepository.getByFlightNumber(flightNumber);
-				route.setDeparture(input.getDeparture());
-				route.setDestination(input.getDestination());
-				route.setDisabled(input.getDisabled());
-				route.setSignature(input.getSignature());
-				route.setBookingDate(input.getBookingDate());
+	public Route updateRoute(String flightNumber, UpdateRouteInput input) {
+		Route route = routeRepository.getByFlightNumber(flightNumber);
+
+		route.setFlightNumber(input.getFlightNumber());
+		route.setDeparture(input.getDeparture());
+		route.setDestination(input.getDestination());
+		route.setDisabled(input.getDisabled());
+		route.setSignature(input.getSignature());
+		route.setBookingDate(input.getBookingDate());
+
 		return routeRepository.save(route);
 	}
+
 //	@Transactional
 //	public Route createRoute(CreateRouteInput input) {
 //		return routeRepository.save(input.toRoute());
@@ -38,6 +40,5 @@ private RouteRepository routeRepository;
 //		routeRepository.delete((Route) routeRepository.findByFlightNumber(flightNumber));
 //	return 1L;
 //	}
-
 
 }
